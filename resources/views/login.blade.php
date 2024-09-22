79fdb4fbfce78e48fa228d6a8e53e03b67e72368
aScavarelli <ascavarelli12@gmail.com>
2024-09-22 18:53:13 -0300

    Vista de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,48 @@
-@include("base.header")
+<!DOCTYPE html>
+<html lang="en">
 
-<h2>Login</h2>
-<form action="/login" method="post">
-    @csrf
-    Nombre: <input type="text" name="email"> <br>
-    Password: <input type="password" name="password"> <br>
-    <input type="submit" value="Iniciar sesion">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/img/icono-logoUptoday.png">
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>Backoffice UpToday</title>
+</head>
 
+<body>
 
-</form>
+    <div class="login">
 
-@if(session("failed"))
-<b>Credenciales invalidas</b> <br>
-@endif
+        <div class="imagen_login">
+            <img src="/img/logo-uptoday.png" alt="Imagen de UpToday">
+        </div>
+
+        <h2>Login</h2>
+        <form action="/login" method="post">
+            @csrf
+            <input type="text" name="email" class="input_login" placeholder="Email"> <br>
+            <input type="password" name="password" class="input_login" placeholder="Contraseña"> <br>
+            <button type="submit">Iniciar sesión</button>
+        </form>
+    </div>
+    <br>
+    <div class="mensaje_invalido">
+        @if(session("failed"))
+        <b>Credenciales inválidas</b> <br>
+        @endif
+    </div>
+
+    <script>
+        const mensajeError = document.querySelector('.mensaje_invalido');
+        if (mensajeError) {
+            mensajeError.style.display = 'block';
+
+            setTimeout(() => {
+                mensajeError.style.display = 'none';
+            }, 3000);
+        }
+    </script>
+</body>
+
+</html>
